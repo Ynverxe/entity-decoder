3eccc3e59df116a8d540119deb992ff98c5a4c01
Ynverxe <nahubar65@gmail.com>
2023-11-02 20:47:53 -0300

    refactor!: remove spawnPosition field

--- src/main/java/com/github/ynverxe/entitydecoder/event/EntityDeserializationEvent.java
+++ src/main/java/com/github/ynverxe/entitydecoder/event/EntityDeserializationEvent.java
@@ -1,7 +1,6 @@
 package com.github.ynverxe.entitydecoder.event;
 
 import com.github.ynverxe.entitydecoder.EntityFactory;
-import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.event.trait.EntityEvent;
 import net.minestom.server.instance.Instance;
@@ -12,13 +11,11 @@ public class EntityDeserializationEvent implements EntityEvent {
   private final @NotNull EntityFactory factory;
   private final @NotNull Instance spawningInstance;
   private final @NotNull Entity entity;
-  private @NotNull Pos spawnPosition;
 
-  public EntityDeserializationEvent(@NotNull EntityFactory factory, @NotNull Instance spawningInstance, @NotNull Entity entity, @NotNull Pos spawnPosition) {
+  public EntityDeserializationEvent(@NotNull EntityFactory factory, @NotNull Instance spawningInstance, @NotNull Entity entity) {
     this.factory = factory;
     this.spawningInstance = spawningInstance;
     this.entity = entity;
-    this.spawnPosition = spawnPosition;
   }
 
   public EntityFactory caller() {
@@ -38,11 +35,4 @@ public class EntityDeserializationEvent implements EntityEvent {
     return entity;
   }
 
-  public void setSpawnPosition(@NotNull Pos spawnPosition) {
-    this.spawnPosition = spawnPosition;
-  }
-
-  public Pos spawnPosition() {
-    return spawnPosition;
-  }
 }
