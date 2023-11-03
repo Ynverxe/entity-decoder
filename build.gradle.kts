0b8762e4536002dd6cc890bbd4de145c9233b185
Ynverxe <nahubar65@gmail.com>
2023-11-02 21:29:52 -0300

    ci: add maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("maven-publish")
     id("java-library")
 }
 
@@ -6,10 +7,23 @@ group = "com.github.ynverxe"
 version = "0.0.1-beta"
 
 repositories {
+    gradlePluginPortal()
     mavenCentral()
     maven("https://jitpack.io")
 }
 
 dependencies {
     compileOnly("com.github.Minestom.Minestom:Minestom:c496ee357")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
 }
